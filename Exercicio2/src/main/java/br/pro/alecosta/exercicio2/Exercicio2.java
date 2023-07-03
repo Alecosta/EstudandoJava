8338357e7982a81e399e3b7c151027cc346a8ab4
AleCosta <alecostabr@gmail.com>
2023-07-03 15:34:30 -0300

    Ex02

---
+++ Exercicio2/src/main/java/br/pro/alecosta/exercicio2/Exercicio2.java
@@ -0,0 +1,24 @@
+package br.pro.alecosta.exercicio2;
+
+/**
+ *
+ * @author alecosta
+ */
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        
+        int numero1 = 10;
+        int numero2 = 2;
+        
+        int soma  = numero1 + numero2;
+        int subtracao  = numero1-numero2;
+        int divisao  = numero1 / numero2;
+        int multiplica  = numero1 * numero2;
+        
+        System.out.println("a Soma de "+numero1+" + "+numero2+" é "+soma );
+        System.out.println("a Substracao de "+numero1+" - "+numero2+" é "+subtracao );
+        System.out.println("a Multiplicação de "+numero1+" * "+numero2+" é "+multiplica );
+        System.out.println("a Divisão de "+numero1+" / "+numero2+" é " +divisao);
+    }
+}
