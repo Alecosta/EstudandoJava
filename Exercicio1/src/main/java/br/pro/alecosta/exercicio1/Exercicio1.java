6939c60eb0c5a6e7213b3348d7492fcaf4130b85
AleCosta <alecostabr@gmail.com>
2023-07-03 15:13:30 -0300

    Ex01

---
+++ Exercicio1/src/main/java/br/pro/alecosta/exercicio1/Exercicio1.java
@@ -0,0 +1,14 @@
+package br.pro.alecosta.exercicio1;
+/**
+ *
+ * @author alecosta
+ */
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 15;
+        int resultado = numero1 + numero2;
+        System.out.println("a Soma de "+numero1+" + "+numero2+" é igual a: "+resultado );
+    }
+}
