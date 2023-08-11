d095cadc067845eba7815e241b128a7ce6300cee
AleCosta <alecostabr@gmail.com>
2023-08-11 18:53:03 -0300

    adicionado Listener

--- Projeto/TodoApp/app/src/main/java/view/MainScreen.java
+++ Projeto/TodoApp/app/src/main/java/view/MainScreen.java
@@ -8,6 +8,8 @@ import controller.ProjectController;
 import controller.TaskController;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.List;
 import javax.swing.DefaultListModel;
 import model.Project;
@@ -315,6 +317,13 @@ public class MainScreen extends javax.swing.JFrame {
         // TODO add your handling code here:
         ProjectDialogScreen projectDialogScreen = new ProjectDialogScreen(this, rootPaneCheckingEnabled);
         projectDialogScreen.setVisible(true);
+        
+        projectDialogScreen.addWindowListener(new WindowAdapter() {
+           public void windowClosed(WindowEvent e) {
+               loadProjects();
+           }
+        
+        });        
     }//GEN-LAST:event_jLabelProjectsAddMouseClicked
 
     private void jLabelTasksAddMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelTasksAddMouseClicked
